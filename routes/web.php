d585cdbfed3b4dff14a125f6820f3f232579564b
Raulntjj <raulntjj@gmail.com>
2024-06-03 14:41:23 -0300

    DS 1.0.0

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return [
-        'Para acessar a documentação navege por host/api/documentation',
+        "Para acessar a documentação da API basta apenas digitar o sufixo '/api/documentation' após seu host ou loopback da máquina",
         'Exemplo: http://127.0.0.1:8000/api/documentation',
-        'Para uso de teste, utilize localhost ou o endereço de loopback da sua máquina',
+        "Lembre-se de executar o comando 'php artisan migrate' para criar o banco de dados na máquina e o comando 'php artisan db:seed' para criar uma seed de dados (opcional)",
         'Laravel' => app()->version()
     ];
 });
